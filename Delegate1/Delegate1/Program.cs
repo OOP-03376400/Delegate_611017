f6c9922c3bcb4a562f84f5a97b28567e713d1176
koson trachu <koson.trachu@gmail.com>
2018-10-17 01:43:08 +0700

    add new console project

---
+++ Delegate1/Delegate1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate1
+{
+    delegate void Mydelegate(string v);
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Mydelegate mydel = new Mydelegate(Print);
+            mydel += Print;
+
+            Print("Hello World.");
+            mydel("Hello Mars");
+        }
+
+        static void Print(string v)
+        {
+            Console.WriteLine(v);    
+        }
+
+    }
+}
